28b36f91fa09cfda55ab58173e940e47a5845b8e
Alekstremo <a01571443@tec.mx>
2025-05-02 04:28:51 -0600

    burndown semi-arreglado

--- frontend/src/contexts/sprintdatacontext.tsx
+++ frontend/src/contexts/sprintdatacontext.tsx
@@ -5,6 +5,7 @@ import { createContext, useContext, useEffect, useState } from "react"
 interface BurndownData {
   duration_days: number
   total_story_points: number
+  remaining_story_points: number
 }
 
 interface TeamMember {
@@ -69,9 +70,9 @@ export const SprintDataProvider = ({ children }: { children: React.ReactNode }) 
       const response = await fetch(`${apiURL}/api/burndown?projectId=${project_id}`)
       const data = await response.json()
 
-      const { duration_days, total_story_points, team_members } = data
+      const { duration_days, total_story_points, remaining_story_points, team_members } = data
 
-      const burndown: BurndownData = { duration_days, total_story_points }
+      const burndown: BurndownData = { duration_days, total_story_points, remaining_story_points }
 
       setBurndownData(burndown)
       setTeamMembers(team_members || [])
