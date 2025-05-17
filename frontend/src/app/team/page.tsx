84995ff2ce0fb9979f230806962add2f16bbf82f
Louis Loewen <144394298+louisloewen@users.noreply.github.com>
2025-05-16 18:24:03 -0600

    Added team view & team detailed view
    Just front-end

--- frontend/src/app/team/page.tsx
+++ frontend/src/app/team/page.tsx
@@ -3,35 +3,29 @@
 import Navbar from "@/components/NavBar";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import TeamsView from "./components/teamview";
 
 export default function TeamPage() {
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
+  const [loading, setLoading] = useState(true)
+  const router = useRouter()
 
   useEffect(() => {
-    const userId = localStorage.getItem("userId");
+    const userId = localStorage.getItem("userId")
     if (!userId) {
-      router.push("/login");
+      router.push("/login")
     } else {
-      setLoading(false);
+      setLoading(false)
     }
-  }, [router]);
+  }, [router])
 
   if (loading) {
-    return null; 
+    return null
   }
-  
+
   return (
     <>
-    <Navbar projectSelected={true} />
-    <main className="min-h-screen py-10 bg-[#EBE5EB]/30">        
-      <div className="container mx-auto px-4">
-        <h1 className="text-2xl font-bold text-gray-800">Team</h1>
-        <p className="mt-2 text-gray-600">
-          Welcome to your Teams! This is a placeholder page.
-        </p>
-      </div>
-      </main>
+      <Navbar projectSelected={true} />
+      <TeamsView />
     </>
-  );
+  )
 }
