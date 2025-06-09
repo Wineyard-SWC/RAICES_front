882b7c3a42af9953f36ae50c471195a337201152
Abdiel <131422484+AbdielFritsche@users.noreply.github.com>
2025-06-09 05:25:54 -0600

    HU-EQ-03 Test Succesfully Executed

---
+++ frontend/cypress/e2e/sprint3/Team/HU-EQ-03.cy.ts
@@ -0,0 +1,25 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.login();
+    cy.wait(10000);
+    cy.login();
+    cy.wait(10000);
+
+    cy.contains('h2', 'EcoHuerto Urbano Comunitario - Plataforma de Gestión').click();
+    cy.wait(4000);
+
+    cy.contains('button','Team').click();
+    cy.wait(6000);
+
+    cy.contains('h3', 'Equipo Pruebas Cypress')
+      .parent() 
+      .find('button.text-gray-400.hover\\:text-red-500')  // Seleccionamos el botón que tiene la clase `hover:text-red-500`
+      .click();
+
+    cy.contains('button','Delete Team').click();
+
+
+    cy.contains('h3', 'Equipo Pruebas Cypress')
+      .should('not.exist');
+  })
+})
