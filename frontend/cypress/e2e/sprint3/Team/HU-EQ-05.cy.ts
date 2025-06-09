a7d21dc626a8544a6612057fe7c56239f92a4e0e
Abdiel <131422484+AbdielFritsche@users.noreply.github.com>
2025-06-09 05:35:26 -0600

    HU-EQ-05 Test Succesfully Executed

---
+++ frontend/cypress/e2e/sprint3/Team/HU-EQ-05.cy.ts
@@ -0,0 +1,24 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.login();
+    cy.wait(10000);
+    cy.login();
+    cy.wait(10000);
+
+    cy.contains('h2', 'EcoHuerto Urbano Comunitario - Plataforma de Gestión').click();
+    cy.wait(4000);
+
+    cy.contains('button','Team').click();
+    cy.wait(6000);
+
+    cy.contains('h3','Project Team').click();
+    cy.wait(10000);
+
+    cy.contains('h2','Team Metrics')
+      .should('exist');
+
+    cy.contains('h2','Team Members')
+      .should('exist');
+
+  })
+})
