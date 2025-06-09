25499a928c1ec65b26dcca24826d9dc50de4b59b
Abdiel <131422484+AbdielFritsche@users.noreply.github.com>
2025-06-09 05:21:36 -0600

    HU-EQ-04 Test Succesfully Executed

---
+++ frontend/cypress/e2e/sprint3/Team/HU-EQ-04.cy.ts
@@ -0,0 +1,26 @@
+describe('User can not delete a team when canceling the confirmation modal', () => {
+  it('User can not delete a team when canceling the confirmation modal', () => {
+    
+    cy.login();
+    cy.wait(10000);
+    cy.login();
+    cy.wait(10000);
+
+    cy.contains('h2', 'EcoHuerto Urbano Comunitario - Plataforma de Gestión').click();
+    cy.wait(4000);
+
+    cy.contains('button','Team').click();
+    cy.wait(6000);
+
+    cy.contains('h3', 'Equipo Pruebas Cypress')
+      .parent() 
+      .find('button.text-gray-400.hover\\:text-red-500')  // Seleccionamos el botón que tiene la clase `hover:text-red-500`
+      .click();
+
+    cy.contains('button','Cancel').click()
+
+
+    cy.contains('h3', 'Equipo Pruebas Cypress')
+      .should('exist');
+  })
+})
