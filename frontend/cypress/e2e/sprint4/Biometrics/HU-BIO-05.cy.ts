874cd31a75012cb43feb229e2d87dde64bc894bb
Abdiel <131422484+AbdielFritsche@users.noreply.github.com>
2025-06-09 22:10:31 -0600

    HU-BIO-02,03,04,05 Tests Succesfully Executed

--- frontend/cypress/e2e/sprint4/Biometrics/HU-BIO-05.cy.ts
+++ frontend/cypress/e2e/sprint4/Biometrics/HU-BIO-05.cy.ts
@@ -1,5 +1,38 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('User can view the graphic data of its heart rate through the history of biometric sessions', () => {
+  it('User can view the graphic data of its heart rate through the history of biometric sessions', () => {
+    cy.loginForBiometrics()
+    cy.wait(10000);
+    cy.loginForBiometrics()
+    cy.wait(10000);
+
+    cy.contains('h2', 'RAICES').click();
+    cy.wait(6000);
+
+    cy.contains('button','View Biometrics').click();
+    cy.wait(4000);
+
+    cy.contains('button', 'Heart Rate').click();
+
+
+    cy.get('div[title*="Session"]')
+      .should('have.length.at.least', 0)
+      .each(($bar, index) => {
+        cy.wrap($bar)
+          .should('have.css', 'background-color', 'rgb(239, 68, 68)')
+          .should('have.css', 'opacity', '0.8')
+          .and(($el) => {
+            const title = $el.attr('title');
+            expect(title).to.match(/Session \d+: -?\d+\.\d+/);
+              
+            const heightStyle = $el.attr('style');
+            expect(heightStyle).to.include('height:');
+              
+            const heightMatch = heightStyle?.match(/height:\s*(\d+(?:\.\d+)?)%/);
+            if (heightMatch) {
+                const height = parseFloat(heightMatch[1]);
+                expect(height).to.be.at.least(0);
+              }
+          });
+      });
   })
 })
